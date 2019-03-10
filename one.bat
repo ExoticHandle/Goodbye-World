c8b846f5a6ba946487b64e72da3de47fc6b96c97
Krzys Grochal <16109223+ExoticHandle@users.noreply.github.com>
2019-03-10 08:44:39 -0400

    Update and rename hentai.bat to one.bat

--- one.bat
+++ one.bat
@@ -2,4 +2,4 @@ echo off
 timeout /t 10 /nobreak
 START /B SystemTrayMessage.exe "Shit this is an error message" /T:"I've done a fucky-wucky" /V:20 /S:186 /W
 timeout /t 10 /nobreak
-start lol.bat
+start two.bat
